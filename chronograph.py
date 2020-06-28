6fa5c25fc005094cbeef47adc111f5769e260ed8
Scott Baker <smbaker@gmail.com>
2020-06-28 11:03:01 -0700

    trying to python-sync the gps clock is a bad idea

--- chronograph.py
+++ chronograph.py
@@ -1,11 +1,35 @@
+from __future__ import print_function
 import pigpio
 import time
 from clock import ClockThread
+from smbpi.gps import GPS
+
+
+class GPSSync(GPS):
+    def __init__(self, pi, clock):
+        super().__init__(pi)
+        self.clock = clock
+
+    def eventGPRMC(self):
+        super().eventGPRMC()
+
+        if time.daylight:
+            timestamp = time.mktime(self.getDateTime().timetuple()) - time.altzone
+        else:
+            timestamp = time.mktime(self.getDateTime().timetuple()) - time.timezone    
+        time.clock_settime(time.CLOCK_REALTIME, timestamp)
+
+        self.clock.indicateGPSSynced()
+
 
 def main():
     pi = pigpio.pi()
+
     threadClock = ClockThread(pi)
     threadClock.start()
+
+    #threadGPS = GPSSync(pi, threadClock)
+    #threadGPS.start()
 
     while True:
         time.sleep(1)
--- clock.py
+++ clock.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import datetime
 import threading
 import time
@@ -8,18 +9,30 @@ class ClockThread(threading.Thread):
     def __init__(self, pi):
         threading.Thread.__init__(self)
         self.pi = pi
+        self.gpsSynced = 0
+        self.lastSecond = 0
         self.vfd = Max6921(pi=pi)
-        self.vfd.setDP(0,True)
-        self.vfd.setDP(2,True)
-        self.vfd.setDP(4,True)
-        self.vfd.setDP(6,True)
+        self.vfd.setDP(0, True)
+        self.vfd.setDP(2, True)
+        self.vfd.setDP(4, True)
+        self.vfd.setDP(6, True)
         self.daemon = True
+
+    def indicateGPSSynced(self):
+        self.gpsSynced = 5
 
     def runOnce(self):
         now = datetime.datetime.now()
         timeStr = now.strftime("%H%M%S") + ("%d" % (now.microsecond / 100000))
+        self.vfd.setLeader(top=(self.gpsSynced > 0))
         self.vfd.displayString(timeStr)
 
+        # update the synced indicator, keep it lit for up to 5 seconds after
+        # the last time we received a gps time packet
+        if (self.lastSecond != now.second):
+            self.lastSecond = now.second
+            if self.gpsSynced > 0:
+                self.gpsSynced = self.gpsSynced - 1
 
     def run(self):
         while True:
@@ -27,4 +40,6 @@ class ClockThread(threading.Thread):
                 self.runOnce()
             except:
                 traceback.print_exc()
+                time.sleep(10)
+
             time.sleep(0.01)
